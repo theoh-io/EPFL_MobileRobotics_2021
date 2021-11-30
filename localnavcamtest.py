440265a489ce2b08c604a542a2a8ad50408158dc
cguillau <camille.guillaume@epfl.ch>
2021-12-01 00:22:22 +0100

    Local nav test small improve, to be continued

--- localnavcamtest.py
+++ localnavcamtest.py
@@ -4,12 +4,12 @@ await tdmclient.notebook.start()
 %%run_python 
 
 y = [0,0]        # powers given to motor wheels with global nav, here TEST 0,0
-state = 0          # 0=global path, 1=obstacle avoidance
+state = 1          # 0=global path, 1=obstacle avoidance
 obst = [0,0,0]     # measurements from left middle right prox sensors
     
 timer_period[0] = 10   # 10ms sampling time
-obstThrL = 10      # low obstacle threshold to switch state 1->0
-obstThrH = 20      # high obstacle threshold to switch state 0->1  
+obstThrL = 5      # low obstacle threshold to switch state 1->0
+obstThrH = 60      # high obstacle threshold to switch state 0->1  
 
 @onevent
 def timer0():
@@ -19,32 +19,30 @@ def timer0():
     obst = [prox_horizontal[0], prox_horizontal[2], prox_horizontal[4]]
     
     # tdmclient does not support yet multiple and/or in if statements:
-    if button_center == 1:
-        state = 1 if state==0 else 0
+    #if button_center == 1:
+    #    state = 1 if state==0 else 0
     
     if state == 0: 
         # switch from goal tracking to obst avoidance if obstacle detected
-        if (obst[2] > obstThrH):
+        if (obst[0] > obstThrH):
             state = 1
-            if (obst[0] > obstThrH):
-                state = 1
-            elif (obst[1] > obstThrH):
-                state = 1
+        elif (obst[2] > obstThrH):
+            state = 1
     elif state == 1:
-        if obst[2] < obstThrL:
-            if obst[0] < obstThrL:
-                if obst[1] < obstThrL : 
-                # switch from obst avoidance to goal tracking if obstacle got unseen
-                state = 0
+        if obst[0] < obstThrL:
+            if obst[2] < obstThrL:
+                if obst[1] < obstThrL:
+                    # switch from obst avoidance to goal tracking if obstacle got unseen
+                    state = 0
                 
-    if  state == 0 :
+    if state == 0:
         # goal tracking: go according to global path with y
         leds_top = [0,0,0]
-        y[0]=0
-        y[1]=0
+        y = [25,25]
         
-    else:
+    elif state == 1:
         leds_top = [30,30,30]
+        
         # obstacle avoidance: ANN
         w_l = [40,  20, -20, -20, -40,  30, -10, 8, 0]
         w_r = [-40, -20, -20,  20,  40, -10, 30, 0, 8]
@@ -52,10 +50,12 @@ def timer0():
         # Scale factors for sensors and constant factor
         sensor_scale = 200
         constant_scale = 20
-    
+        
         x = [0,0,0,0,0,0,0,0,0]
-        x[7] = y[0]//5
-        x[8] = y[1]//5
+        
+        # Memory
+        x[7] = y[0]//20
+        x[8] = y[1]//20
         
         for i in range(7):
             # Get and scale inputs
