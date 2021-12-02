94432fc116ce1dc7e760bab86368393574f4ff00
theo hermann <theo.hermann27@hotmail.fr>
2021-12-02 16:33:16 +0100

    read motors speed

--- control.py
+++ control.py
@@ -6,21 +6,20 @@ switch to PID/ASTOLFI
 
 #import needed
 import numpy as np
-from tdmclient import ClientAsync
+import time
+from tdmclient import ClientAsync, aw
 
-
-#inputs
-#from global nav
-next_point=(1.00,1.00)
-#from filter
-actual_point=(0.00,0.00)
-actual_angle=0.00
 
 #needed variables definition
-#translation: speed approx 20cm/s, need to know the coefficient linking coordinates to meters
-coeff_dist=1
-run_speed=100
-coeff_speed=21.73913043478261
+#motors speed no unit
+motor_speed=100
+#translation: speed approx 3,25 [cm/s]
+real_speed=3.25   #21.73913043478261
+#coeff linking thymio dist to real [coord/cm] (suppose 1 coord = 20cm)
+dist_coord=10 # [cm/coord] coord thymio ref = 10cm
+coeff_dist=dist_coord
+
+
 #rotation
 #rot_coeff=
 rot_speed=100
@@ -35,19 +34,28 @@ def motors(left, right):
         "motor.right.target": [right],
     }
 
+#middle level functions
+def set_motors(left,right,node):
+    aw(node.set_variables(motors(left,right)))
 
-#middle level function
-def forward(next,actual):
+def stopmotors(node):
+    aw(node.set_variables(motors(0,0)))
+
+
+#high level function
+def forward(next,actual,node, client):
     direction=np.subtract(next,actual)
     dist=np.sqrt(np.sum(np.square(direction)))
-    forward_time=(dist*coeff_dist)/(run_speed*coeff_speed)
-    aw(node.set_variables(motors(run_speed,run_speed)))
-    client.sleep(forward_time)
-    stopmotors()
+    #dist: coord, coeff_dist: cm/coord, motor_speed[motor], coeff speed cm/s at 100
+    #print(dist, coeff_dist, motor_speed, real_speed)
+    forward_time=(dist*coeff_dist)/(real_speed) 
+    #print(forward_time)
+    set_motors(motor_speed,motor_speed,node)
+    aw(client.sleep(forward_time))
+    stopmotors(node)
 
 
-def turn(next,actual,actual_angle):
-    global node
+def turn(next,actual,actual_angle, node, client):
     #should we use the circular notation for negative: 2**16-??
     direction=np.subtract(next,actual)
     new_angle=np.arctan2(direction[0],direction[1])
@@ -65,12 +73,22 @@ def turn(next,actual,actual_angle):
     rot_time=(angle_diff)/(rot_speed*coeff_rotspeed)
     if(angle_diff>0):
         aw(node.set_variables(motors(-rot_speed,rot_speed)))
-        client.sleep(rot_time)
+        aw(client.sleep(rot_time))
     elif(angle_diff<0):
         aw(node.set_variables(motors(rot_speed,-rot_speed)))
-        client.sleep(rot_time)
-    stopmotors()
+        aw(client.sleep(rot_time))
+    stopmotors(node)
 
-def stopmotors():
-    global node
-    aw(node.set_variables(motors(0,0)))
+
+def read_motors_speed(node,client):
+    aw(node.wait_for_variables({"motor.left.speed","motor.right.speed"}))
+    aw(client.sleep(0.3))
+    speed=[node.v.motor.left.speed, node.v.motor.right.speed]
+    return speed
+
+#calibration: rotate until on itself
+def calib_rot(node,client):
+    aw(node.set_variables(motors(-rot_speed,rot_speed)))
+ 
+
+    
