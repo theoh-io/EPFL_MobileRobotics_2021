532318292fc8e5251568ebd231962e15dcf78931
theo hermann <theo.hermann27@hotmail.fr>
2021-12-07 12:37:26 +0100

    img calibration

--- Vision.py
+++ Vision.py
@@ -6,19 +6,39 @@ import matplotlib
 from matplotlib.pyplot import imshow
 from matplotlib import pyplot as plt
 
+
+#variables declaration
+iterations_erode=6
+area_size=500 #used for corner detections
+
 #Defining color ranges
 pink_lower=[]
 pink_upper=[]
 lower_red = [0,50,50]
 upper_red = [10,255,255]
-green_lower=[85,50,50]
-green_upper=[135,255,255]
 lower_cyan =[170,50,50]
 upper_cyan =[180,255,255]
+green_lower=np.array([60,40,40]) #only one that we tested
+green_upper=np.array([80,255,255])
 
 
 
 #low level functions (appelées a l'intérieur d'autres fonctions)
+def plot_contours(img,contours,area_size, nb_sides):
+    for cnt in contours :
+        area = cv2.contourArea(cnt)
+   
+    # Shortlisting the regions based on there area.
+    if area > area_size: 
+        approx = cv2.approxPolyDP(cnt, 
+                                  0.009 * cv2.arcLength(cnt, True), True)
+        #print(approx)
+   
+        # Checking if the no. of sides of the selected region.
+        if(len(approx) == nb_sides): 
+            cv2.drawContours(img, [approx], -1, (255, 0, 0), 10)
+    return img
+
 def order_points(pts):
     """
     # initialzie a list of coordinates that will be ordered
@@ -42,7 +62,6 @@ def order_points(pts):
     """
      #sorting points first by the 2nd the coordinate then 1st coordinate
     pts=sorted(pts, key=lambda x: (int(x[1]), int(x[0]))) #topleft,topright,bottomleft,bottomright
-    #vérification du sorting et intervertit si il y a eu une erreur
     #top left x > top right x => erreur et intervertit
     if pts[0][0] > pts[1][0]:
         pts[0], pts[1] = pts[1], pts[0]
@@ -55,34 +74,32 @@ def order_points(pts):
 def four_point_transform(image, ordered_pts):
 	# obtain a consistent order of the points and unpack them
 	# individually
-	(tl, tr, br, bl) = ordered_pts
+    (tl, tr, br, bl) = ordered_pts
 	# compute the width of the new image, which will be the
 	# maximum distance between bottom-right and bottom-left
 	# x-coordiates or the top-right and top-left x-coordinates
-	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-	maxWidth = max(int(widthA), int(widthB))
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
 	# compute the height of the new image, which will be the
 	# maximum distance between the top-right and bottom-right
 	# y-coordinates or the top-left and bottom-left y-coordinates
-	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-	maxHeight = max(int(heightA), int(heightB))
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
 	# now that we have the dimensions of the new image, construct
 	# the set of destination points to obtain a "birds eye view",
 	# (i.e. top-down view) of the image, again specifying points
 	# in the top-left, top-right, bottom-right, and bottom-left
 	# order
-	dst = np.array([
-		[0, 0],
-		[maxWidth - 1, 0],
-		[maxWidth - 1, maxHeight - 1],
-		[0, maxHeight - 1]], dtype = "float32")
+    #conversion needed corner points => np.array
+    ordered_pts=np.array(ordered_pts , dtype = "float32")
+    dst = np.array([[0, 0],[maxWidth - 1, 0],[0, maxHeight - 1],[maxWidth - 1, maxHeight - 1]],dtype = "float32")
 	# compute the perspective transform matrix and then apply it
-	M = cv2.getPerspectiveTransform(ordered_pts, dst)
-	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    M = cv2.getPerspectiveTransform(ordered_pts, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
 	# return the warped image
-	return warped
+    return warped
 
 
 
@@ -104,24 +121,28 @@ def color_mask(imgRGB, color):
     return output_hsv
 
 
-
 #high level functions (celles qu'on appelle dans main)
 def img_calibration(img):
     #input img doit etre en rgb
-    HSV = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-    #applique flou gaussien pour que les contours soit moins durs
-    HSV_blur = cv2.GaussianBlur(HSV, (7, 7), 0)
-    #a voir si on utilise une autre couleur
-    green_mask=cv2.inRange(HSV_blur,green_lower,green_upper)
-    #green_mask[70:350,:]=0 #remove the yellow which is similar to green corners
-    #green_mask[:,100:500]=0 #remove the yellow which is similar to green corners
-    
-    # Identifier les coins et leurs coordonnées
+    img_blur = cv2.GaussianBlur(img, (7, 7), 0)
+    HSV = cv2.cvtColor(img_blur, cv2.COLOR_RGB2HSV)
+    mask=cv2.inRange(HSV,green_lower,green_upper)
+    mask = cv2.erode(mask,None, iterations=iterations_erode)
+    mask = cv2.dilate(mask,None, iterations=iterations_erode)
     #trouve les contours des coins
-    contours, hierarchy = cv2.findContours(green_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) 
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
+    #suppressing false corners
+    initial_length=len(contours)
+    for i in range(len(contours)):
+        backwards_i=initial_length-i-1
+        area = cv2.contourArea(contours[backwards_i])
+        # Shortlisting the regions based on there area.
+        if area < area_size:
+            del contours[backwards_i]
+    #finding corners center
     corner_points = []
     for i in range(len(contours)):
-        if (cv2.contourArea(contours[i]) > 200):
+        if (cv2.contourArea(contours[i]) > area_size):
             mom = cv2.moments(contours[i])
             corner_points.append((int(mom['m10'] / mom['m00']), int(mom['m01'] / mom['m00']))) #centre des carrés
     if len(corner_points) != 4:
