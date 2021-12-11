91d3aac086cee00dd90634ebf92925ec1cbcd701
cguillau <camille.guillaume@epfl.ch>
2021-12-11 16:29:26 +0100

    Local nav with all front sensors

--- local_nav.py
+++ local_nav.py
@@ -4,19 +4,48 @@ def update_state(state,obstThrL,obstThrH, obst):
         if (obst[0] > obstThrH):
             state = 3
             print("change state to local")
+        elif (obst[4] > obstThrH):
+            state = 3
+            print("change state to local")
         elif (obst[2] > obstThrH):
+            state = 3
+            print("change state to local")
+    elif state == 3:
+        if obst[0] < obstThrL:
+            if obst[4] < obstThrL:
+                # switch from obst avoidance to goal tracking if obstacle got unseen
+                state = 2
+                print("change state to global")
+    
+    return state
+
+def update_state2(state,obstThrL,obstThrH, obst):
+    if state == 2: 
+        # switch from goal tracking to obst avoidance if obstacle detected
+        if (obst[2] > obstThrH):
             state = 3
             print("change state to local")
         elif (obst[1] > obstThrH):
             state = 3
             print("change state to local")
+        elif (obst[3] > obstThrH):
+            state = 3
+            print("change state to local")
+        elif (obst[0] > obstThrH):
+            state = 3
+            print("change state to local")
+        elif (obst[4] > obstThrH):
+            state = 3
+            print("change state to local")
     elif state == 3:
-        if obst[0] < obstThrL:
-            if obst[2] < obstThrL:
-                # switch from obst avoidance to goal tracking if obstacle got unseen
-                state = 2
-                print("change state to global")
-    
+        if obst[2] < obstThrL:
+            if obst[1] < obstThrL:
+                if obst[3] < obstThrL:
+                    if obst[0] < obstThrL:
+                        if obst[4] < obstThrL:
+                            # switch from obst avoidance to goal tracking if obstacle got unseen
+                            state = 2
+                            print("change state to global")
     return state
 
 def local_nav(prox_horizontal,y):
